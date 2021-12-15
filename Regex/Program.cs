2c2b27266c3df37e0033adf8a237bd6683423f6e
mc0re <mnikitin0@gmail.com>
2021-12-15 20:55:34 +0100

    Transfer code from PC.

---
+++ Regex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+var rx = new Regex("(?<n1> \\d+) \\s* (?<op> \\+|-|\\*|/) \\s* (?<n2> \\d+)",
+                   RegexOptions.IgnorePatternWhitespace);
+var s = Console.ReadLine();
+var m = rx.Match(s);
+if (!m.Success)
+{
+    Console.WriteLine("No match");
+    return;
+}
+
+Console.WriteLine($"Split input: {m.Groups["n1"].Value} {m.Groups["op"].Value} {m.Groups["n2"].Value}");
